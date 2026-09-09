27b35e3030d2f7b5523e9da6750e60424dac57f3
vanshbilwal-lgtm <vanshbilwal@gmail.com>
2026-09-09 08:42:17 +0530

    Factorial program

---
+++ Factorial.cpp
@@ -0,0 +1,12 @@
+//Factorial program
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5, fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    cout << "Factorial of " << n << " is " << fact << endl;
+    return 0;
+}
